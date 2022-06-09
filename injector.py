4cb606ac6919ca67828cd4235813c8b8c062d2e5
brook <81716228+brooke-a@users.noreply.github.com>
2022-06-09 15:26:29 +0100

    fr
    fr

---
+++ injector.py
@@ -0,0 +1,31 @@
+from pymem import Pymem
+import os
+import subprocess
+
+# Make a DLL injector for Minecraft using Pymem
+MC = subprocess.Popen('Minecraft.Bedrock.exe')
+
+pm = Pymem('Minecraft.Bedrock.exe')
+pm.inject_python_interpreter()
+filepath = os.path.join(os.path.abspath('.'), 'INJECTION.txt')
+filepath = filepath.replace('\\', '\\\\')
+shellcode = """"""
+f = open("{}", 'w')
+f.write("INJECTION")
+f.close()
+pm.write_bytes(0x00400000, shellcode)
+pm.write_bytes(0x00400000, filepath)
+pm.write_bytes(0x00400000, "INJECTION")
+pm.write_bytes(0x00400000, "INJECTION")
+pm.write_bytes(0x00400000, "INJECTION")
+pm.write_bytes(0x00400000, "INJECTION")
+pm.write_bytes(0x00400000, "INJECTION")
+pm.write_bytes(0x00400000, "INJECTION")
+pm.write_bytes(0x00400000, "INJECTION")
+pm.write_bytes(0x00400000, "INJECTION")
+pm.write_bytes(0x00400000, "INJECTION")
+pm.write_bytes(0x00400000, "INJECTION")
+pm.write_bytes(0x00400000, "INJECTION")
+"""""".format(filepath)
+pm.inject_python_shellcode(shellcode)
+MC.kill()
